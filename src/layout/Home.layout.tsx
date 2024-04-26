55077af901f6adb0f896f1679127cb5dd8306691
WilmerEspinal <wilmerespinalvillanueva27@gmail.com>
2024-04-26 16:22:41 -0500

    admin-dash

--- src/layout/Home.layout.tsx
+++ src/layout/Home.layout.tsx
@@ -4,31 +4,59 @@ import IconHome from "../assets/icons/Home.svg";
 import IconTeacher from "../assets/icons/Teacher.svg";
 import IconCalendar from "../assets/icons/Calendar.svg";
 import LinkNav from "@/components/ui/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { MdNotificationsNone } from "react-icons/md";
+
 
 
 const HomeLayout = () => {
     return (
         <>
-        <main>
-            <section className="flex">
-                <section className="h-screen w-2/12 min-w-40 bg-governor-bay-700 text-white">
-                    <h2 className="text-center text-4xl">Logo</h2>
-                    <div className="mt-8 pl-7">
-                        <LinkNav name="Dashboard" imgUrl={IconHome}/>
-                        <LinkNav name="Estudiantes" imgUrl={IconStudent}/>
-                        <LinkNav name="Profesores" imgUrl={IconTeacher}/>
-                        <LinkNav name="Examen" imgUrl={IconStudent}/>
-                        <LinkNav name="Eventos" imgUrl={IconCalendar}/>
-                    </div>
+            <main>
+                <section className="">
+                    <section className="h-16 w-full bg-governor-bay-100 flex">
+                        <div className="flex items-center h-16 bg-governor-bay-700 min-w-64 w-64 justify-center ">
+                            <h1 className="text-4xl text-white">Logo</h1>
+                        </div>
+                        <div className="flex items-center justify-between bg-governor-bay-100 w-full" >
+                            <div>
+                                <h2 className=" text-governor-bay-800 text-2xl font-medium">Dashboard</h2>
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <button>
+                                <MdNotificationsNone className=" text-3xl bg-white rounded-full text-governor-bay-500" />
+                                </button>
+                            
+                                <button>
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                </button>
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <section className="flex">
+                        <section className="h-screen min-w-64 w-64  bg-governor-bay-700 text-white">
+                            <div className="mt-8 pl-7">
+                                <LinkNav name="Dashboard" imgUrl={IconHome} />
+                                <LinkNav name="Estudiantes" imgUrl={IconStudent} />
+                                <LinkNav name="Profesores" imgUrl={IconTeacher} />
+                                <LinkNav name="Examen" imgUrl={IconStudent} />
+                                <LinkNav name="Eventos" imgUrl={IconCalendar} />
+                            </div>
+                        </section>
+                        <section className="h-screen w-full bg-governor-bay-100">
+
+                        </section>
+                    </section>
+
                 </section>
-                <section className="w-screen h-screen bg-governor-bay-100">
-                    
+                <section>
+                    <Outlet />
                 </section>
-            </section>
-            <section>
-                <Outlet/>
-            </section>
-        </main>
+            </main>
         </>
     )
 }
